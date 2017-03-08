6466e1b7659a396444395808cf73492b8d2574be
erika johansson <erika.b.johansson@gmail.com>
2017-03-08 18:32:54 +0100

    testing w gallery

---
+++ gallery.js
@@ -0,0 +1,24 @@
+
+var origText;
+
+function upDate(previewPic){
+	
+    var bigImage = document.getElementById("image");
+    
+    origText = bigImage.innerHTML;
+    
+    var smallPic = previewPic.src;
+    
+    bigImage.style.backgroundImage = "url('" + smallPic + "')";
+    
+    bigImage.innerHTML = previewPic.alt;
+}
+
+function unDo(){
+ 
+	var bigImage = document.getElementById("image");
+ 
+    bigImage.style.backgroundImage = "url('')";
+ 
+    bigImage.innerHTML = origText;
+}
